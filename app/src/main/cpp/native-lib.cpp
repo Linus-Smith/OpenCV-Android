89580a1d933a4832347f15ec614a27220592bcae
caihui yang <caihuiyang@caihuideMacBook-Pro.local>
2017-05-15 00:35:12 +0800

    init OpenCv project

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,44 @@
 #include <jni.h>
 #include <string>
-
+#include <opencv2/opencv.hpp>
+#include <bits/stdc++.h>
 extern "C"
 jstring
-Java_com_linus_opencv_1android_MainActivity_stringFromJNI(
+Java_com_linus_opencv_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+
+extern "C"
+JNIEXPORT jintArray JNICALL Java_com_linus_opencv_MainActivity_getCrayImage(
+        JNIEnv *env,
+        jobject type,
+        jintArray pixels_,
+        jint w, jint h) {
+
+    jint *pixels = env->GetIntArrayElements(pixels_, NULL);
+    // TODO
+    if(pixels==NULL){
+        return NULL;
+    }
+    cv::Mat imgData(h, w, CV_8UC4, pixels);
+    uchar *ptr = imgData.ptr(0);
+    for (int i = 0; i < w * h; i++) {
+        int grayScale = (int) (ptr[4 * i + 2] * 0.299 + ptr[4 * i + 1] * 0.587
+                               + ptr[4 * i + 0] * 0.114);
+        ptr[4 * i + 1] = (uchar) grayScale;
+        ptr[4 * i + 2] = (uchar) grayScale;
+        ptr[4 * i + 0] = (uchar) grayScale;
+    }
+
+    int size = w * h;
+    jintArray result = env->NewIntArray(size);
+    env->SetIntArrayRegion(result, 0, size, pixels);
+    env->ReleaseIntArrayElements(pixels_, pixels, 0);
+    return result;
+
+
 }
